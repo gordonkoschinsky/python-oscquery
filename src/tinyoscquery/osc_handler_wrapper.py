3fd5007bad76b1855d983de4e497bce00769c495
Frank <nospam_github@theatris.de>
2025-12-26 21:30:36 +0100

    Add very basic callback wrapper for python-osc

--- src/tinyoscquery/osc_handler_wrapper.py
+++ src/tinyoscquery/osc_handler_wrapper.py
@@ -1,13 +1,19 @@
 import logging
 
+import pythonosc
+from pythonosc.dispatcher import Dispatcher
+
 from tinyoscquery.shared.osc_path_node import OSCPathNode
 
 
 logger = logging.getLogger(__name__)
 
-class OSCHandlerWrapper:
-    def __init__(self, node:OSCPathNode):
+class OSCCallbackWrapper:
+    def __init__(self, node:OSCPathNode, dispatcher: Dispatcher, callback: callable):
         self.node = node
+        self.callback = callback
+        self.handler = dispatcher.map(node.full_path, self)
 
     def __call__(self, *args, **kwargs):
         logger.debug(f"OSCHandlerWrapper {self.node.full_path} called with args={args} kwargs={kwargs}")
+        return self.callback(*args, **kwargs)
